b98ac7f3992e2d4c11c5b91759881b2e60b8696c
Gómez Diego <gomezdiegoivan15mail.com>
2023-02-21 13:28:11 -0300

    Initial commit

---
+++ CV.js
@@ -0,0 +1,10 @@
+/**Botones url*/
+function newDoc2() {
+  window.open("https://www.google.com/intl/es/gmail/about/")
+}
+function newDoc3() {
+  window.open("https://es-la.facebook.com/")
+}
+function newDoc4() {
+  window.open("https://www.google.com.ar/maps/")
+}
